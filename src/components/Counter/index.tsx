35089bd055fc950fa7893f6ed5ddde14ad483b49
Michel Wene <michelwene@hotmail.com>
2023-02-24 17:18:52 -0300

    feat: added counter of products Header

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,12 +1,16 @@
 import * as S from "./styles";
 import { useNavigate } from "react-router-dom";
+import { useProductsContext } from "context/ProductsProvider";
 
 export default function Counter() {
+	const { products } = useProductsContext();
 	const navigate = useNavigate();
 	return (
 		<S.Container>
 			<S.Description>Total de produtos cadastrados:</S.Description>
-			<S.Counter onClick={() => navigate("/produtos")}>0</S.Counter>
+			<S.Counter onClick={() => navigate("/produtos")}>
+				{products.length > 99 ? "99+" : products.length}
+			</S.Counter>
 		</S.Container>
 	);
 }
