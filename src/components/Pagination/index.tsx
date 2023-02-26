ab43b94f28b9402935e10992af0b58ec8e64a6da
Michel Wene <michelwene@hotmail.com>
2023-02-26 11:38:04 -0300

    feat: search for categories and price

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -19,7 +19,10 @@ export function Pagination({
 				<S.Button onClick={prevPage} disabled={currentPage === 1}>
 					<S.LeftIcon />
 				</S.Button>
-				<S.Button onClick={nextPage} disabled={currentPage === totalPages}>
+				<S.Button
+					onClick={nextPage}
+					disabled={currentPage === totalPages || totalPages === 0}
+				>
 					<S.RightIcon />
 				</S.Button>
 			</S.Content>
--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -8,15 +8,17 @@ import CardProduct from "components/CardProduct";
 import EmptyMessage from "components/EmptyMessage";
 import { Pagination } from "components/Pagination";
 import { useState } from "react";
+import useDebounce from "utils/useDebounce";
+import Input from "components/Input";
+import { removeSpecialCharacters } from "utils/removeSpecialCharacters";
 
 export default function Products() {
 	const [search, setSearch] = useState("");
+	const debouncedSearch = useDebounce(search, 700);
+
 	const [page, setPage] = useState(1);
 	const [value, toggle] = useToggle(false);
 	const { products } = useProductsContext();
-	console.log("products:", products);
-
-	const totalPages = Math.ceil(products.length / 10);
 
 	const handleNextPage = () => {
 		if (page < totalPages) {
@@ -30,18 +32,51 @@ export default function Products() {
 		}
 	};
 
+	const filteredProducts = products.filter((product) => {
+		const categoryWithoutAccent = removeSpecialCharacters(product.category!);
+		return (
+			product.name.toLowerCase().includes(debouncedSearch.toLowerCase()) ||
+			categoryWithoutAccent
+				.toLowerCase()
+				.includes(removeSpecialCharacters(debouncedSearch).toLowerCase()) ||
+			product.price!.toString().includes(debouncedSearch.toLowerCase())
+		);
+	});
+
 	const productsPerPage = products.slice((page - 1) * 10, page * 10);
+	const productsFilteredPerPage = filteredProducts.slice(
+		(page - 1) * 10,
+		page * 10
+	);
+	const productsToRender = debouncedSearch
+		? productsFilteredPerPage
+		: productsPerPage;
+
+	const totalPages = debouncedSearch
+		? Math.ceil(filteredProducts.length / 10)
+		: Math.ceil(products.length / 10);
+
+	const handleSearch = (text: string) => {
+		setPage(1);
+		setSearch(text);
+	};
 
 	return (
 		<>
 			<S.Container>
 				<S.PageTitle>Lista de produtos</S.PageTitle>
-
+				<Input
+					name="search"
+					id="search"
+					label="Pesquisar"
+					value={search}
+					onChange={handleSearch}
+				/>
 				<S.ProductsList>
-					{products.length === 0 && (
+					{productsToRender.length === 0 && (
 						<EmptyMessage text="Não foi encontrado produtos cadastrados 😥" />
 					)}
-					{productsPerPage.map((product) => (
+					{productsToRender.map((product) => (
 						<CardProduct product={product} key={product.id} />
 					))}
 				</S.ProductsList>
